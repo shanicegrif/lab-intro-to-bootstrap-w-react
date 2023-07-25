81eb797c3c42c957e8da12be25c51c283805cb2a
K. Rafalski <kr2174@caa.columbia.edu>
2023-07-25 17:23:39 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import postData from "./data/posts.json";
+
+function App() {
+  console.log(postData);
+  return (
+    <main>
+      <button className="btn">hi</button>
+    </main>
+  );
+}
+
+export default App;
